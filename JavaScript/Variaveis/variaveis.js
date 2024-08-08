12bb4c893f605360a3b8a868bfbb5387eda1f533
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-08 20:59:27 -0300

    Javascript referente a aula sobre váriaveis

---
+++ JavaScript/Variaveis/variaveis.js
@@ -0,0 +1,16 @@
+//String
+var texto = "Curso de JavaScript"
+
+//Number 
+var numeroInteiro = -7
+
+var numeroFracionario = 10.25
+
+//Boolean
+var teste = true //false
+
+alert(numeroFracionario)
+
+document.write(texto)
+
+console.log(numeroInteiro)
