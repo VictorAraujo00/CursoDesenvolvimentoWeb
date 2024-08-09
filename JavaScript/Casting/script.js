e6b68c2cff44e3260d1792c4458819174072aeee
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-09 17:44:42 -0300

    Javascript referente a aula sobre Casting

---
+++ JavaScript/Casting/script.js
@@ -0,0 +1,12 @@
+var variavel1 = prompt("Digite algum número")
+var variavel2 = prompt("Digite outro número")
+var variavel3 = 10
+var variavel4 = 20
+
+variavel1 = parseInt(variavel1)
+variavel2 = parseFloat(variavel2)
+
+console.log(variavel1)
+console.log(variavel2)
+
+document.write(variavel3.toString() + variavel4.toString())
