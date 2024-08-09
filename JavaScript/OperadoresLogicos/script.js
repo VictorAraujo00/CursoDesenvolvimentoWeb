1a5b7596d5ce3c9176ff7a728b0cd3a4f8d0f3bf
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-09 17:46:20 -0300

    Javascript referente a aula sobre Operadores Lógicos

---
+++ JavaScript/OperadoresLogicos/script.js
@@ -0,0 +1,12 @@
+if(2 == 2 && 3 >= 1 && 'a' == 'b'){
+
+	document.write("Entrou")
+
+}
+else{
+	document.write("Não entrou")
+}
+
+var resultado = 4 == 2 ? 'Verdadeiro' : 'Falso'
+
+document.write(resultado)
