c26d41be9ea432e87c05d9de7322ac5dcd4e4b59
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-19 09:57:46 -0300

    Javascript referente a aula sobre Funções matemáticas

---
+++ JavaScript/FuncoesMatematicas/script.js
@@ -0,0 +1,20 @@
+var x = Math.ceil(10.380)
+document.write(x)
+document.write("<br>")
+
+var x2 = Math.floor(10.380)
+document.write(x2)
+document.write("<br>")
+
+var x3 = Math.round(10.580)
+document.write(x3)
+document.write("<br>")
+
+var x4 = Math.round(10.380)
+document.write(x4)
+document.write("<br>")
+
+var x5 = Math.random()
+document.write(x5)
+document.write("<br>")
+
