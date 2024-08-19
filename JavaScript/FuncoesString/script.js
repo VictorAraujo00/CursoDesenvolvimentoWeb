6dee2a5ac48f80edc17b3c13201cb36f014ca982
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-19 09:55:17 -0300

    Javascript referente a aula sobre funções string

---
+++ JavaScript/FuncoesString/script.js
@@ -0,0 +1,8 @@
+var nome = ' Jorge Sant Ana '
+document.write(nome.length)
+document.write(nome.charAt(0))
+document.write(nome.indexOf('g'))
+document.write(nome.replace('Jorge', 'Victor'))
+document.write(nome.substr(6, 4))
+document.write("<br>")
+document.write("-" + nome.trim() + "-")
