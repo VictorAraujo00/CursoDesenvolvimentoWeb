fbe684f5ec9f776b4f2c996145f9253783115226
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-26 09:22:39 -0300

    Javascript referente a aula sobre Array Multidimensional

---
+++ JavaScript/ArrayMultidimensional/script.js
@@ -0,0 +1,11 @@
+var listas_coisas = Array()
+
+listas_coisas['frutas'] = Array()
+listas_coisas['frutas'][0] = 'Banana'
+listas_coisas['frutas'][1] = 'Maçã'
+
+listas_coisas['pessoas'] = Array()
+listas_coisas['pessoas'][0] = 'José'
+listas_coisas['pessoas'][1] = 'Maria'
+
+console.log(listas_coisas)
