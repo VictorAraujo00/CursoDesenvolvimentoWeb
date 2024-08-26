d0277e84fb959c88fdbee99e5d88b058d81ada89
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-26 09:26:21 -0300

    Javascript referente a aula sobre Metodos de Inclusão de Exclusão

---
+++ JavaScript/MetodosInclusaoExclusao/script.js
@@ -0,0 +1,22 @@
+var lista_frutas = Array()
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Maçã'
+
+//Incluir elemento do final do array
+
+lista_frutas.push('Uva')
+
+//Incluir elemento no incio do array
+
+lista_frutas.unshift('Morango')
+
+//Excluir elemento no final do array
+
+lista_frutas.pop()
+
+//Excluir elemento no começo do array
+
+lista_frutas.shift()
+
+console.log(lista_frutas)
