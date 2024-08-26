cc9ae9b980a64a54da4ac8fab84cd28f7e0d45f8
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-26 09:29:02 -0300

    Javascript referente a aula sobre metódos de pesquisa

---
+++ JavaScript/MetodoPesquisa/script.js
@@ -0,0 +1,8 @@
+var lista_frutas = Array()
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Maçã'
+lista_frutas[2] = 'Morango'
+lista_frutas[3] = 'Uva'
+
+console.log(lista_frutas.indexOf('Morango'))
