9b2439f42ed2a0f414d32122a46d50459fdad997
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-26 09:19:54 -0300

    Javascript referente a aula sobre array

---
+++ JavaScript/Array/script.js
@@ -0,0 +1,15 @@
+/*var lista_frutas = Array()
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Maçã'
+lista_frutas[2] = 'Morango'
+lista_frutas[3] = 'Uva'
+
+console.log(lista_frutas[1])*/
+
+var lista_fruta = Array('Banana', 'Maçã', 'Morango', 'Uva')
+console.log(lista_fruta)
+
+for (var i = 0; i < lista_fruta.length; i++) {
+	document.write(lista_fruta[i])
+}
