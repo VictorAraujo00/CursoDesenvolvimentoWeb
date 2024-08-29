1881be424ca1802431b63a8fb9419fa66002e36c
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-29 07:36:15 -0300

    Javascript referente a aula sobre Tabuada

---
+++ JavaScript/Tabuada/script.js
@@ -0,0 +1,14 @@
+for(var i = 1; i <= 10; i++){
+
+	for(var j = 1; j <= 10; j++){
+
+		result = i * j
+
+		document.write(i + ' x ' + j + ' = ' + result + '<br>')
+
+
+	}
+
+	document.write('<hr>')
+
+}
