cc96d668c73320249e3dfcbd6aa47a4c1102adb4
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-08-29 07:40:32 -0300

    Javascript referente a aula sobre ForEach

---
+++ JavaScript/ForEach/script.js
@@ -0,0 +1,10 @@
+var lista_funcionarios = ['Viviane', 'Rosângela', 'Miguel', 'Lucas', 'Fernanda']
+
+lista_funcionarios.forEach(function(valor, indice, array){
+
+	console.log(indice)
+	console.log(valor)
+	console.log(array)
+
+
+})
